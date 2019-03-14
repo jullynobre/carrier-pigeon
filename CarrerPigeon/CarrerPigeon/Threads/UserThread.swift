37b21b86a46c2a3ba8526b3bbc5d7e7e467aa80c
jullynobre <jullynobre1@gmail.com>
2019-03-14 20:25:10 -0300

    Add iOS project

---
+++ CarrerPigeon/CarrerPigeon/Threads/UserThread.swift
@@ -0,0 +1,9 @@
+//
+//  UserThread.swift
+//  CarrerPigeon
+//
+//  Created by Jully Nobre on 14/03/19.
+//  Copyright © 2019 Jully Nobre. All rights reserved.
+//
+
+import Foundation
---
+++ CarrerPigeon/CarrerPigeon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarrerPigeon
+//
+//  Created by Jully Nobre on 14/03/19.
+//  Copyright © 2019 Jully Nobre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
