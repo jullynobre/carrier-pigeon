2229f733bed333b6d266875cdd20b78dc05e747c
jullynobre <jullynobre1@gmail.com>
2019-03-24 19:19:19 -0300

    Add auxiliar methods to boxView

--- CarrerPigeon/CarrerPigeon/Components/Box/BoxView.swift
+++ CarrerPigeon/CarrerPigeon/Components/Box/BoxView.swift
@@ -15,14 +15,6 @@ class BoxView: UIView {
 	@IBOutlet weak var progressLabel: UILabel!
 	@IBOutlet weak var progressBar: UIProgressView!
 	
-	/*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		commonInit()
@@ -38,14 +30,38 @@ class BoxView: UIView {
 		addSubview(contentView)
 		contentView.frame = self.bounds
 		contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        
+		
         configureProgressBar()
+		
 	}
-    
+	
     func configureProgressBar() {
         self.progressBar.transform = CGAffineTransform(scaleX: 1, y: 5)
         
         self.progressBar.layer.cornerRadius = 5.0
         self.progressBar.clipsToBounds = true
     }
+	
+	func setCapacity(capacity: Int) {
+		self.progressLabel.text = "0/\(capacity)"
+	}
+	
+	func addToBox(quantity: Int) {
+		let progessArr = self.progressLabel.text!.components(separatedBy: "/")
+		let itens = Int(progessArr[0])! + quantity
+		let capacity = Int(progessArr[1])!
+		
+		self.progressLabel.text = "\(itens)/\(capacity)"
+		self.progressBar.setProgress(Float(quantity/capacity), animated: true)
+	}
+	
+	func removeFromBox(quantity: Int) {
+		let progessArr = self.progressLabel.text!.components(separatedBy: "/")
+		let itens = Int(progessArr[0])! - quantity
+		let capacity = Int(progessArr[1])!
+		
+		self.progressLabel.text = "\(itens)/\(capacity)"
+		self.progressBar.setProgress(Float(quantity/capacity), animated: true)
+	}
+	
 }
