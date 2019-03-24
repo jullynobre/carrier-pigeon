3dd698a2483c4dab6b4a71f33d6051aab86a2667
jullynobre <jullynobre1@gmail.com>
2019-03-24 15:49:54 -0300

    Add modal controllers

---
+++ CarrerPigeon/CarrerPigeon/Controllers/WriterModalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WriterModalViewController.swift
+//  CarrerPigeon
+//
+//  Created by Jully Nobre on 24/03/19.
+//  Copyright © 2019 Jully Nobre. All rights reserved.
+//
+
+import UIKit
+
+class WriterModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
